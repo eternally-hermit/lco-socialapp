d076fec233d20bc95f300ce9b0f53befc48347a3
H3RMIT <shubhamhalder0@gmail.com>
2022-06-25 21:50:07 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const format = require('date-format');
+const app = express();
+
+const PORT = 4000;
+
+app.get('/',(req,res) => {
+    res.status(200).send('<h3 >Hello From Express...!</h3>');
+})
+
+app.get('/api/v1/instagram',(req,res) => {
+    const instaSocial = {
+        username: "hermitsocial_instagram",
+        followers: 40,
+        follows:10,
+        date: format.asString("dd:MM - hh:mm:ss",new Date())
+    };
+    res.status(200).json(instaSocial);
+})
+
+app.get('/api/v1/facebook',(req,res) => {
+    const instaSocial = {
+        username: "hermitsocial_fb",
+        followers: 45,
+        follows:112,
+        date: format.asString("dd:MM - hh:mm:ss",new Date())
+    };
+    res.status(200).json(instaSocial);
+})
+
+app.get('/api/v1/linkedin',(req,res) => {
+    const instaSocial = {
+        username: "hermitsocial_linkedin",
+        followers: 445,
+        follows:121,
+        date: format.asString("dd:MM - hh:mm:ss",new Date())
+    };
+    res.status(200).json(instaSocial);
+})
+
+app.get('/api/v1/:token',(req,res) => {
+    console.log(req.params.token);
+    res.status(200).json({param:req.params.token})
+})
+
+app.listen(PORT,() => {
+    console.log(`Server is running on ${PORT}`);
+})
